c86e32e273a423ec84ecc1a8a4573618b3199c6c
Francisco dos Passos <francisco.nedir@squidit.com.br>
2023-12-20 14:50:12 -0300

    feat: ensure Login calls validation with correct email

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -5,16 +5,23 @@ import Context from '@/presentation/contexts/form/form-context'
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> & { name: string }
 
 const Input: React.FC<Props> = (props: Props) => {
-  const { errorState } = useContext(Context)
-  const error = errorState[props.name]
+  const { state, setState } = useContext(Context)
+  const error = state[`${props.name}Error`]
   const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
     event.target.readOnly = false
   }
+  const handleChange = (event: React.FocusEvent<HTMLInputElement>): void => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.value
+    })
+  }
   const getStatus = (): string => '🔴'
   const getTitle = (): string => error
+
   return (
     <div className={Styles.inputWrap}>
-      <input {...props} readOnly onFocus={enableInput}/>
+      <input {...props} data-testid={props.name} readOnly onFocus={enableInput} onChange={handleChange}/>
       <span data-testid={`${props.name}-status`} title={getTitle()} className={Styles.status}>{getStatus()}</span>
     </div>
   )
