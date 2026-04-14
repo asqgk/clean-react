ea6db29c4a095a23657ab7cb7509089fabe32663
franciscosquid <francisco.nedir@squidit.com.br>
2026-04-14 14:47:29 -0300

    refactor: move required-field-validation

--- src/validation/validators/required-field/required-field-validation.spec.ts
+++ src/validation/validators/required-field/required-field-validation.spec.ts
@@ -1,11 +1,11 @@
-import { RequiredFieldValidation } from '@/validation/required-field/required-field-validation'
+import { RequiredFieldValidation } from '@/validation/validators/required-field/required-field-validation'
 import { RequiredFieldError } from '@/validation/errors'
 import faker from 'faker'
 
 const makeSut = (): RequiredFieldValidation => new RequiredFieldValidation(faker.database.column())
 
 describe('RequiredField Validation', () => {
-  test('Should return error error if field is empty', () => {
+  test('Should return error if field is empty', () => {
     const sut = makeSut()
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFieldError())
