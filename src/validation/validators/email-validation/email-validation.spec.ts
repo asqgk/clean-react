3f565ecf2dd3ecb5103d458b79e73056ea5d0e3e
franciscosquid <francisco.nedir@squidit.com.br>
2026-04-14 14:51:59 -0300

    refactor: update email validation regex for improved accuracy

--- src/validation/validators/email-validation/email-validation.spec.ts
+++ src/validation/validators/email-validation/email-validation.spec.ts
@@ -2,12 +2,12 @@ import { InvalidFieldError } from '@/validation/errors'
 import { EmailValidation } from './email-validation'
 import faker from 'faker'
 
-const makeSut = (): EmailValidation => new EmailValidation('email')
+const makeSut = (): EmailValidation => new EmailValidation(faker.random.word())
 
 describe('Email Validation', () => {
   test('Should return error if email is invalid', () => {
     const sut = makeSut()
-    const error = sut.validate('email')
+    const error = sut.validate(faker.random.word())
     expect(error).toEqual(new InvalidFieldError())
   })
 
