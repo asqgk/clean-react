a428b08d096f6f880a983c9ac7936effd98fec71
franciscosquid <francisco.nedir@squidit.com.br>
2026-04-13 21:41:09 -0300

    fix: update RequiredFieldValidation to return null for non-empty fields

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,10 +1,17 @@
 import { RequiredFieldValidation } from '@/validation/required-field/required-field-validation'
 import { RequiredFieldError } from '@/validation/errors'
+import faker from 'faker'
 
 describe('RequiredField Validation', () => {
   test('Should return error error if field is empty', () => {
     const sut = new RequiredFieldValidation('email')
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFieldError())
+  })
+
+  test('Should return falsy if field is not empty', () => {
+    const sut = new RequiredFieldValidation('email')
+    const error = sut.validate(faker.random.word())
+    expect(error).toBeFalsy()
   })
 })
