612e2a5bfb05d2d40a93cbd90455dae22f5fc725
franciscosquid <francisco.nedir@squidit.com.br>
2026-04-14 15:44:53 -0300

    fix: update error message in ValidationComposite test to reflect correct validation failure

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -4,14 +4,15 @@ import { FieldValidationSpy } from '../test/mock-field-validation'
 describe('Validation Composite', () => {
   test('Should return an error if any validation fails', () => {
     const fieldValidationSpy = new FieldValidationSpy('any_field')
+    fieldValidationSpy.error = new Error('first_error')
     const fieldValidationSpy2 = new FieldValidationSpy('any_field')
-    fieldValidationSpy2.error = new Error('any_error')
+    fieldValidationSpy2.error = new Error('second_error')
 
     const sut = new ValidationComposite([
       fieldValidationSpy,
       fieldValidationSpy2
     ])
     const error = sut.validate('any_field', 'any_value')
-    expect(error).toBe('any_error')
+    expect(error).toBe('first_error')
   })
 })
