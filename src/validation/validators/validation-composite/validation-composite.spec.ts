2eac286adc613479901c05ed18f43ae5cd661711
franciscosquid <francisco.nedir@squidit.com.br>
2026-04-14 15:54:17 -0300

    refactor: update makeSut function to accept fieldName parameter for improved test flexibility

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,15 +1,16 @@
 import { ValidationComposite } from './validation-composite'
 import { FieldValidationSpy } from '../test/mock-field-validation'
+import faker from 'faker'
 
 type SutTypes = {
   sut: ValidationComposite
   fieldValidationsSpy: FieldValidationSpy[]
 }
 
-const makeSut = (): SutTypes => {
+const makeSut = (fieldName: string): SutTypes => {
   const fieldValidationsSpy = [
-    new FieldValidationSpy('any_field'),
-    new FieldValidationSpy('any_field')
+    new FieldValidationSpy(fieldName),
+    new FieldValidationSpy(fieldName)
   ]
 
   const sut = new ValidationComposite(fieldValidationsSpy)
@@ -21,10 +22,19 @@ const makeSut = (): SutTypes => {
 
 describe('Validation Composite', () => {
   test('Should return an error if any validation fails', () => {
-    const { sut, fieldValidationsSpy } = makeSut()
-    fieldValidationsSpy[0].error = new Error('first_error')
-    fieldValidationsSpy[1].error = new Error('second_error')
-    const error = sut.validate('any_field', 'any_value')
-    expect(error).toBe('first_error')
+    const fieldName = faker.database.column()
+    const { sut, fieldValidationsSpy } = makeSut(fieldName)
+    const errorMessage = faker.random.words()
+    fieldValidationsSpy[0].error = new Error(errorMessage)
+    fieldValidationsSpy[1].error = new Error(faker.random.words())
+    const error = sut.validate(fieldName, faker.random.words())
+    expect(error).toBe(error)
+  })
+
+  test('Should return falsy if all validations succeed', () => {
+    const fieldName = faker.database.column()
+    const { sut } = makeSut(fieldName)
+    const error = sut.validate(fieldName, faker.random.words())
+    expect(error).toBeFalsy()
   })
 })
