4b848d550e7071b42317039d039f67f322a95034
franciscosquid <francisco.nedir@squidit.com.br>
2026-04-14 15:26:27 -0300

    feat: add unit tests for MinLengthValidation class

---
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -0,0 +1,19 @@
+import { InvalidFieldError } from '@/validation/errors'
+import { MinLengthValidation } from './min-length-validation'
+import faker from 'faker'
+
+const makeSut = (): MinLengthValidation => new MinLengthValidation(faker.database.column(), 5)
+
+describe('Min Length Validation', () => {
+  test('Should return error if field length is less than min length', () => {
+    const sut = makeSut()
+    const error = sut.validate(faker.random.alphaNumeric(4))
+    expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('Should return falsy if field length is equal or greater than min length', () => {
+    const sut = makeSut()
+    const error = sut.validate(faker.random.alphaNumeric(5))
+    expect(error).toBeFalsy()
+  })
+})
