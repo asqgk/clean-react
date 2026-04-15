ad231b44c255763248268b2d064c8756f52d080f
franciscosquid <francisco.nedir@squidit.com.br>
2026-04-15 11:04:39 -0300

    feat: create index file for exporting validation modules

---
+++ src/validation/validators/index.ts
@@ -0,0 +1,4 @@
+export * from './email/email-validation'
+export * from './builder/validation-builder'
+export * from './required-field/required-field-validation'
+export * from './validation-composite/validation-composite'
